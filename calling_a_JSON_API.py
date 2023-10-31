44a9fb0ac39b90c7ef7ab8530c4cafedd1c9d5f3
Roy Contreras Benitez <roy7474@gmail.com>
2023-10-30 23:07:38 -0400

    while loop has been added to eliminate error of not input

--- calling_a_JSON_API.py
+++ calling_a_JSON_API.py
@@ -105,3 +105,14 @@ serviceurl = 'http://py4e-data.dr-chuck.net/json?'
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: 
+        break
+
+    # parms = dict()
+    parms = {'address' : address}
+    #if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
