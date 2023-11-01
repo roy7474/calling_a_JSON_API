060c3ed0aad3cb326b8daa2fac08d47a45672a40
Roy Contreras Benitez <roy7474@gmail.com>
2023-11-01 19:33:47 -0400

    try different approach using the original sample code and doing minimal modification and obtatined better results

--- calling_a_JSON_API.py
+++ calling_a_JSON_API.py
@@ -93,6 +93,11 @@ arizona state university
 Make sure to enter the name and case exactly as above and enter the place_id and your Python code below. 
 Hint: The first seven characters of the place_id are "ChIJD1Q ...'''
 
+
+
+
+
+'''
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
@@ -108,7 +113,7 @@ ctx.verify_mode = ssl.CERT_NONE
 
 #loop to prompt for locations
 while True:
-    address = input('Enter location: ')
+    address = input('Enter location:')
     if len(address) < 1: 
         break
 
@@ -132,3 +137,53 @@ while True:
         print('place id:', place_id)
     else:
         print("place ID not found for", address)
+    '''
+
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    pid = js['results'][0]['place_id']
+    print('Place id:', pid)
