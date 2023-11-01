6c23690852ef7ae38e8c56786072f8cafebef7ee
Roy Contreras Benitez <roy7474@gmail.com>
2023-11-01 17:01:17 -0400

    if loop has been added to check if place_id is found in the JSON response

--- calling_a_JSON_API.py
+++ calling_a_JSON_API.py
@@ -106,6 +106,7 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+#loop to prompt for locations
 while True:
     address = input('Enter location: ')
     if len(address) < 1: 
